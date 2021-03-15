1f7364167b751e81d57caac86200fbabb962c8b6
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-16 03:23:49 +0500

    update entry based on user input

--- src/components/dashBoard/loggedIn/DataList.tsx
+++ src/components/dashBoard/loggedIn/DataList.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import EditIcon from "@material-ui/icons/Edit";
 import { gql, useQuery } from "@apollo/client";
 import { useMutation } from "@apollo/client";
 import { IconButton } from "@material-ui/core";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 const GET_CRUDS = gql`
   query GetCruds {
@@ -28,11 +32,18 @@ const UpdateCrudMutation = gql`
   }
 `;
 
+// Type Defination
+interface EntryProps {
+  entry: string;
+}
+
 export const DataList = () => {
   const { loading, error, data, refetch } = useQuery(GET_CRUDS);
   const [deleteCurd] = useMutation(DeleteCurdMutation);
   const [updateCrud] = useMutation(UpdateCrudMutation);
-  console.log(data);
+  const [editing, setEditing] = useState(false);
+  const [editedText, setEditedText] = useState("");
+  const [editedId, setEditedId] = useState("");
   if (data.cruds.length === 0) {
     return (
       <div className='taskScreen taskScreenE'>
@@ -57,9 +68,56 @@ export const DataList = () => {
     });
     await refetch();
   };
+  const initialValues: EntryProps = {
+    entry: editedText,
+  };
   return (
     <div>
       <div className='taskScreen'>
+        {editing && (
+          <div>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={Yup.object({
+                entry: Yup.string()
+                  .min(3, "Must be 3 characters or more")
+                  .max(15, "Must be 15 characters or less")
+                  .required("Kindly add some Text"),
+              })}
+              onSubmit={(values) => {
+                updateCrudSubmit(editedId, values.entry);
+                setEditing(false);
+              }}
+            >
+              <Form className='formControl1'>
+                <div className='fieldsDiv1'>
+                  <Field
+                    as={TextField}
+                    variant='outlined'
+                    className='fields'
+                    name='entry'
+                    label='Your Entry here'
+                    helperText={
+                      <ErrorMessage name='entry'>
+                        {(msg) => <span className='error'>{msg}</span>}
+                      </ErrorMessage>
+                    }
+                  />
+                </div>
+                <div className='btnDivF'>
+                  <Button
+                    style={{ color: "white" }}
+                    variant='contained'
+                    className='green'
+                    type='submit'
+                  >
+                    <EditIcon />
+                  </Button>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+        )}
         {data.cruds.map((crud) => (
           <div key={crud.id}>
             <div className={crud.done ? "taskEntryA" : "taskEntry"}>
@@ -77,7 +135,9 @@ export const DataList = () => {
               <div
                 className='pinned'
                 onClick={() => {
-                  updateCrudSubmit(crud.id, "testing edit");
+                  setEditedText(crud.text);
+                  setEditedId(crud.id);
+                  setEditing(true);
                 }}
               >
                 <IconButton>
