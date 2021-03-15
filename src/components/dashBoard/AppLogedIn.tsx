a415f52f3048ba4b30302b4e156336fa652331df
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-15 07:28:33 +0500

    Step13:- Update Dashboard with input form
    1:- Installed
    	a:installed Formik Forms
    		i:- installed forms using "yarn add formik"
    		ii:- installed Yup using "yarn add yup"
    		iii:- installed Yup types using "yarn add @types/yup"
    2:- Updated
    	a:- src/components/dashboard/AppLogedIn.tsx to add a input form
    3:- Created
    	a:- src/components/dashboard/appLogedIn.css to add styling for input form

--- src/components/dashBoard/AppLogedIn.tsx
+++ src/components/dashBoard/AppLogedIn.tsx
@@ -1,10 +1,67 @@
 import React from "react";
 import { AppHead } from "./AppHead";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import "./appLogedIn.css";
+
+// Type Defination
+interface EntryProps {
+  entry: string;
+}
+
+// Initial Values
+const initialValues: EntryProps = {
+  entry: "",
+};
 
 export const AppLogedIn = () => {
   return (
     <div>
       <AppHead />
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={Yup.object({
+            entry: Yup.string()
+              .min(3, "Must be 3 characters or more")
+              .max(15, "Must be 15 characters or less")
+              .required("Kindly add some Text"),
+          })}
+          onSubmit={(values) => {
+            alert(values.entry);
+          }}
+        >
+          <Form className='formControl1'>
+            <div className='fieldsDiv1'>
+              <Field
+                as={TextField}
+                variant='outlined'
+                className='fields'
+                name='entry'
+                label='Your Entry here'
+                helperText={
+                  <ErrorMessage name='entry'>
+                    {(msg) => <span className='error'>{msg}</span>}
+                  </ErrorMessage>
+                }
+              />
+            </div>
+            <div className='btnDivF'>
+              <Button
+                style={{ color: "white" }}
+                variant='contained'
+                className='green'
+                type='submit'
+              >
+                <AddCircleOutlineIcon />
+              </Button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
