07e742839a127956e46c8322ea99d39b7b115ad5
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-15 23:44:00 +0500

    Step22:- fetch data from fauna and add data

--- functions/crud/crud.js
+++ functions/crud/crud.js
@@ -1,4 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
+var client = new faunadb.Client({ secret: process.env.FAUNA });
 
 const typeDefs = gql`
   type Query {
@@ -17,21 +21,37 @@ const cruds = {};
 let crudIndex = 0;
 const resolvers = {
   Query: {
-    cruds: (parent, args, { user }) => {
+    cruds: async (parent, args, { user }) => {
       if (!user) {
         return [];
       } else {
-        return Object.values(cruds);
+        const results = await client.query(
+          q.Paginate(q.Match(q.Index("curds_by_user"), user))
+        );
+        return results.data.map(([ref, text]) => ({
+          id: ref.id,
+          text,
+        }));
       }
     },
   },
   Mutation: {
-    addCrud: (_, { text }) => {
-      console.log("adding");
-      crudIndex++;
-      const id = `key-${crudIndex}`;
-      cruds[id] = { id, text, done: false };
-      return cruds[id];
+    addCurd: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to insert todos");
+      }
+      const results = await client.query(
+        q.Create(q.Collection("curds"), {
+          data: {
+            text,
+            owner: user,
+          },
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
   },
 };
