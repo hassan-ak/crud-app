2b4aaf8b46d17bc6bf2880e9adcaeee14b528e31
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-03-15 18:18:01 +0500

    Step15:- connect app to GraphQl using Apollo client
    1:- Installed
    	a:- Appolo client using "yarn add @apollo/client"
    2:- Updated
    	a:- gatsby-browser.js to
    		i:- wrap the elemnt in appolo provider
    		ii:- make a call to deployed qraphql api
    	c:- functions/todos/todos.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,24 @@
 import "./src/styles/global.css";
 import wrapRootElement from "./wrap-root-element";
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "/.netlify/functions/crud",
+  }),
+});
 
-const _wrapRootElement = wrapRootElement;
+const _wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {wrapRootElement({ element })}
+    </ApolloProvider>
+  );
+};
 export { _wrapRootElement as wrapRootElement };
